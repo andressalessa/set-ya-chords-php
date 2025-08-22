606d0265ea59adda452a5a06b65fefe245ce9842
Andressa Lessa <s.andressalessa@gmail.com>
2025-08-22 18:17:53 -0300

    Adding scroll to cifra view

--- views/cifra.view.php
+++ views/cifra.view.php
@@ -7,14 +7,14 @@
             <i class="bi bi-file-earmark-plus text-emerald-300 group-active:text-cyan-300"></i>
         </a>
     </div>
-    <div class="group flex mt-4 px-4">
+    <!-- <div class="group flex mt-4 px-4">
         <a
             href="/salvar-pdf?id=<?= $cifra->id ?>"
             class="border-1 border-emerald-300 group-active:border-cyan-300 px-2 py-1 rounded-xl">
             Salvar PDF
             <i class="bi bi-file-earmark-pdf text-emerald-300 group-active:text-cyan-300"></i>
         </a>
-    </div>
+    </div> -->
 </div>
 <div class="flex flex-col mt-4">
     <div class="flex flex-col mx-auto justify-center items-center">
@@ -26,4 +26,21 @@
     <div class="ml-4 mt-4 whitespace-pre leading-normal">
         <?php echo $cifra->cifra ?>
     </div>
+</div>
+<div class="fixed right-4 top-1/2 transform -translate-y-1/2 flex flex-col items-center z-10">
+    <button
+        id="speed-up"
+        class=" text-emerald-300 p-1 rounded-lg flex items-center"
+        data-speed="10">
+        <i class="bi bi-plus-square-fill text-lg"></i>
+    </button>
+    <button id="toggle-scroll" class="bg-emerald-300 text-slate-900 px-2 py-1 rounded-lg flex items-center">
+        <i class="bi bi-play-fill"></i>
+    </button>
+    <button
+        id="speed-down"
+        class="text-emerald-300 p-1 rounded-lg flex items-center"
+        data-speed="10">
+        <i class="bi bi-dash-square-fill"></i>
+    </button>
 </div>
